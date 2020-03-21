ee1385599f7ce14a54930f7bcea9ba84b5a32bcf
joonvena <joonvena@gmail.com>
2020-03-21 17:04:34 +0200

    Added location tracking with Geolocation watchPosition

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import auth from '@react-native-firebase/auth';
+import Geolocation from 'react-native-geolocation-service';
 import LoginView from './views/LoginView';
-import {View, Text} from 'react-native';
+import {View, Text, PermissionsAndroid} from 'react-native';
 
 const App: () => React$Node = () => {
   const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState('');
+  const [error, setError] = useState('');
+  const [position, setPosition] = useState({latitude: 0, longitude: 0});
 
   const registerUser = async (username, password) => {
     try {
@@ -29,6 +32,25 @@ const App: () => React$Node = () => {
   };
 
   useEffect(() => {
+    const granted = PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (granted) {
+      const watchId = Geolocation.watchPosition(
+        pos => {
+          setError('');
+          setPosition({
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+          });
+        },
+        e => setError(e.message),
+      );
+      return () => Geolocation.clearWatch(watchId);
+    }
+  }, []);
+
+  useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
   }, []);
@@ -45,7 +67,7 @@ const App: () => React$Node = () => {
 
   return (
     <View>
-      <Text>Welcome!</Text>
+      <Text>Hello {position.latitude}</Text>
     </View>
   );
 };
