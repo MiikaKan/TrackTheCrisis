4f5481926887af41a9c6ba33409d24d9d264c7ea
Miika Kanerva <miika.kanerva@aalto.fi>
2020-03-21 19:10:07 +0200

    places API added

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import auth from '@react-native-firebase/auth';
 import messaging from '@react-native-firebase/messaging';
 import Geolocation from 'react-native-geolocation-service';
 import LoginView from './views/LoginView';
 import {View, Text, PermissionsAndroid} from 'react-native';
 
+const API_KEY = '';
+
 const App: () => React$Node = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState('');
   const [error, setError] = useState('');
   const [position, setPosition] = useState({latitude: 0, longitude: 0});
+
+  const filterPlacesByTypes = places => {
+    const typesToFilterOut = ['locality'];
+
+    return places.filter(place => {
+      let count = 0;
+      place.types.forEach(type =>
+        typesToFilterOut.forEach(filterOutType => {
+          if (type == filterOutType) count++;
+        }),
+      );
+
+      return count < 1 ? place : false;
+    });
+  };
+
+  const fetchNearbyPlaces = pos => {
+    const base = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+    axios
+      .get(
+        `${base}?location=${pos.coords.latitude},${pos.coords.longitude}&radius=100&key=${API_KEY}`,
+      )
+      .then(response => {
+        const results = response.data.results;
+
+        const filteredByTypes = filterPlacesByTypes(results);
+        const filteredRelevantFields = filteredByTypes.map(item => {
+          return {
+            name: item.name,
+            types: item.types,
+            id: item.id,
+            icon: item.icon,
+          };
+        });
+
+        console.error(filteredRelevantFields);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   const registerUser = async (username, password) => {
     try {
@@ -50,9 +94,10 @@ const App: () => React$Node = () => {
             latitude: pos.coords.latitude,
             longitude: pos.coords.longitude,
           });
+          fetchNearbyPlaces(pos);
         },
         e => setError(e.message),
-        {enableHighAccuracy: true, distanceFilter: 1, interval: 10000},
+        {enableHighAccuracy: true, distanceFilter: 100, interval: 10000},
       );
       return () => Geolocation.clearWatch(watchId);
     }
